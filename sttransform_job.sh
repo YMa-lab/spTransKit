bd0f1aef2b0a718dd65762dde18831758d3780ff
vsrajani03 <69733012+vsrajani03@users.noreply.github.com>
2024-05-16 23:42:29 -0400

    Delete sttransform_job.sh

--- sttransform_job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -n 1
-#SBATCH -N 1
-#SBATCH --mem=128G
-#SBATCH -t 2:00:00
-#SBATCH -o output.out
-#SBATCH --job-name stTransform
-
-python3 src/transformations.py --counts data/151673/matrix.csv --coordinates data/151673/coordinates.txt --genes data/151673/marker_genes.csv --pathways data/151673/marker_pathways.txt
