ffaa81ce71202677bad5788e489f50c808b68c45
vsrajani03 <vsrajani03@gmail.com>
2025-05-02 11:06:30 -0400

    package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from sptranskit import __version__
+
+setup(
+    name='sptranskit',
+    version=__version__,
+
+    url='https://github.com/YMa-lab/spTransKit',
+    author='Vivek Rajani',
+    author_email='vsrajani03@gmail.com',
+
+    py_modules=['sptranskit'],
+)
